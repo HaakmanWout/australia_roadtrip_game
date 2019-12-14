2a215b9df6f6fa8936bd2883ada7fc7c9f5b7d07
Wout Haakman <wouthaakman@hotmail.com>
2019-12-14 13:28:44 +0100

    feat: First working version

--- scripts/game.js
+++ scripts/game.js
@@ -1,39 +1,50 @@
-document.addEventListener("click", function(){
-    if (posy >= 52.5) {
+document.addEventListener("click", function(event) {
+    if (event.target.closest('#australian_soil') && posy >= 52.5) {
         force = std_force;
     }
 });
+
+var game_over = true;
+var score = 0;
 
 var std_force = 3;
 var posy = 52.5;
 var force = 0;
 var demean = 0.1;
 
+var score_text = document.getElementById("score");
 var honda_crv = document.getElementById("honda_crv");
-var stats = document.getElementById("stats");
 var enemies = document.getElementById("enemies");
+
+var sydney = document.getElementById("sydney");
+var canberra = document.getElementById("canberra");
+var melbourne = document.getElementById("melbourne");
 
 var enemy_list = [];
 
-function calculateForce() {
+function calculate_force() {
     force = Math.max(force - demean, -std_force);
     
     posy = Math.min(52.5, posy - force);
 }
 
-function pushKangaroo() {
-    enemy_list.push({enemy_name:"kangaroo", posx: 120, posy: 52.5});
+function push_kangaroo(xpos) {
+    enemy_list.push({enemy_name:"kangaroo", posx: xpos, posy: 55});
+}
+
+function intersects(rect1, rect2) {
+    return !(rect1.right < rect2.left || 
+        rect1.left > rect2.right || 
+        rect1.bottom < rect2.top || 
+        rect1.top > rect2.bottom)
 }
 
 function draw() {
-    calculateForce();
+    calculate_force();
+    score += 1;
 
-    var hondaBB = honda_crv.getBoundingClientRect();
-    
-    stats.innerHTML = "Stats: " + Number((hondaBB.x).toFixed(2)) + ", " + Number((hondaBB.y).toFixed(2))
-        + ", " + Number((hondaBB.width).toFixed(2)) + ", " + Number((hondaBB.height).toFixed(2));
-
-    honda_crv.style.transform = "translate3d(25vh, " + posy +"vh, 0)";
+    score_text.innerHTML = "Score: " + score;
+    honda_crv.style.transform = "translate3d(10vw, " + posy + "vh, 0)";
 
     enemies.innerHTML = "";
     for (var i = 0; i < enemy_list.length; i++) {
@@ -41,19 +52,37 @@ function draw() {
         if (enemy_list[i].posx <= -20) {
             enemy_list[i].posx = 120;
         }
-        enemies.innerHTML += "<div id=\"" + enemy_list[i].enemy_name + "\" style=\"transform:translate3d(" + enemy_list[i].posx + "vw, " + enemy_list[i].posy + "vh, 0)\"></div>";
+        enemies.innerHTML += "<div id=\"" + enemy_list[i].enemy_name + "\" class=\"" + i + "\" style=\"transform:translate3d(" + enemy_list[i].posx + "vw, " + enemy_list[i].posy + "vh, 0)\"></div>";
+
+        var enemyBB = document.getElementsByClassName(i)[0].getBoundingClientRect();
+        if (intersects(honda_crv.getBoundingClientRect(), enemyBB)) {
+            game_over = true;
+        }
     }
 }
 
 function loop(timestamp) {
     var progress = timestamp - lastRender
   
-    draw()
+    if (!game_over) {
+        draw()
+    }
   
     lastRender = timestamp
     window.requestAnimationFrame(loop)
   }
 
+  function restart() {
+      game_over = false;
+      score = 0;
+
+      enemy_list = [];
+  
+      push_kangaroo(120);
+      push_kangaroo(200);
+  }
+
+  restart();
+
   var lastRender = 0
-  pushKangaroo()
   window.requestAnimationFrame(loop)
